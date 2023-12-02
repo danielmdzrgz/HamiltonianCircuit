23373ccfadb7c1a3f6d956221f88856fe8e62e96
Julio <julior@keytrends.ai>
2023-12-02 22:24:53 +0000

    Added graph visualization and yaml format

---
+++ hamiltoniancircuit/input_reader.py
@@ -0,0 +1,24 @@
+from rich.console import Console
+
+import yaml
+import networkx as nx
+import matplotlib.pyplot as plt
+
+console = Console()
+
+def run():
+    console.print("Hamiltonian Circuit", style="bold red")
+
+    with open('graph.yaml') as grafo_yaml:
+        grafo_datos = yaml.safe_load(grafo_yaml)
+
+    G = nx.Graph()
+
+    G.add_nodes_from(grafo_datos['vertexes'])
+    # Añadir las aristas al grafo
+    G.add_edges_from(grafo_datos['edges'])
+
+    plt.figure(figsize=(8, 6))
+    nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, edge_color='black', linewidths=1, font_size=15)
+    plt.title("Graph", fontsize=20)
+    plt.show()
