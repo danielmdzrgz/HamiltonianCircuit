85b080fb77c73ebea47b5d48ba220bc6d217174f
Julio <julioeruzicka@gmail.com>
2023-12-10 21:48:25 +0000

    Improved algorithm + cleaned

--- hamiltoniancircuit/gadget_algorithm.py
+++ hamiltoniancircuit/gadget_algorithm.py
@@ -1,26 +1,36 @@
-from __future__ import annotations
-from typing import Dict, List, Literal, Union, Tuple
-from typing import Dict, List, Literal, Union, Tuple
+from typing import List, Literal, Union
 from uuid import UUID
-from hamiltoniancircuit.gadget import Gadget
 from rich.console import Console
+from hamiltoniancircuit.gadget import Gadget
+
+console = Console()
 
 def gadget_algorithm(gadgets: List[Gadget], nodes: List[UUID]) -> List[Gadget]:
-  for node in nodes:
-    current_gadget: int = -1
-    previous_gadget: int = -1
-    previous_side: Union[Literal["Right"], Literal["Left"]] = "Left"
-    for i in range(len(gadgets)):
-      if gadgets[i].contains_node(node):
-        current_side: Union[Literal["Right"], Literal["Left"]]
-        if gadgets[i].left_.node_id == node:
-          current_side = "Left"
-        else:
-          current_side = "Right"
-        current_gadget = i
-        if previous_gadget != -1:
-          gadgets[previous_gadget].join(previous_side, gadgets[current_gadget], current_side, 6)
-        previous_gadget = current_gadget
-        previous_side = current_side
-    
-  return gadgets
+    """This function connects the gadgets that belong to the same node."""
+    for node in nodes:
+        console.print(f"Node: {node}")
+        current_gadget: Union[Gadget, None] = None
+        previous_gadget: Union[Gadget, None] = None
+        previous_side: Union[Literal["Right"], Literal["Left"]] = "Left"
+
+        for gadget in gadgets:
+            if not gadget.contains_node(node):
+                continue
+
+            current_side = "Right" if gadget.right_.node_id == node else "Left"
+            current_gadget = gadget
+
+            if not previous_gadget:
+                previous_gadget = current_gadget
+                previous_side = current_side
+                continue
+
+            console.print(previous_gadget)
+            console.print(previous_side)
+            console.print(current_gadget)
+            console.print(current_side)
+            previous_gadget.join(previous_side, current_gadget, current_side)
+            previous_gadget = current_gadget
+            previous_side = current_side
+
+    return gadgets
--- main.py
+++ main.py
@@ -18,21 +18,26 @@ def test() -> None:
     node_3 = uuid4()
     node_4 = uuid4()
 
-    gadget1 = Gadget(node_3, node_1)
-    gadget2 = Gadget(node_4, node_1)
-    gadget3 = Gadget(node_2, node_1)
-    gadget4 = Gadget(node_4, node_2)
+    gadget1 = Gadget(node_1, node_3)
+    gadget2 = Gadget(node_3, node_4)
+    gadget3 = Gadget(node_1, node_4)
+    gadget4 = Gadget(node_1, node_2)
+    gadget5 = Gadget(node_2, node_4)
 
     #gadget.join("Left", gadget2, "Left")
     #gadget2.join("Left", gadget3, "Left")
     #gadget.join("Right", gadget3, "Right")
 
-    gadget_algorithm([gadget1, gadget2, gadget3, gadget4], [node_1, node_2, node_3, node_4])
+    gadgets = [gadget1, gadget2, gadget3, gadget4, gadget5]
+    nodes = [node_1, node_2, node_3, node_4]
 
-    console.print(gadget1)
-    console.print(gadget2)
-    console.print(gadget3)
-    console.print(gadget4)
+    for i, node in enumerate(nodes):
+        console.print(f"Node {i+1}: {node}")
+
+    connected_gadgets = gadget_algorithm(gadgets, nodes)
+
+    for gadget in connected_gadgets:
+        console.print(gadget)
 
     # selector = Selector()
     # selector.connect_gadget(gadget)
@@ -52,7 +57,6 @@ def test() -> None:
     # selector_list = tools.make_connections(2, [gadget, gadget2, gadget3])
     # for selector in selector_list:
     #     console.print(selector)
-
 
 # Gadget ID: 1, R : [6: G-3], L : [6: G-2]
 # Gadget ID: 2, R : [], L : [1: G-1, 6: G-3]
